cfd3cf515ec2ae4df63b7e9a6451c0c4f9aafbd8
Declan <declan@supertree.co>
2023-07-26 12:16:37 +0900

    update

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Sock_console_server
 {
+    // 서버의 흐름: 소켓 생성 -> bind()로 소켓에 주소 할당 -> Socket이 연결 시도를 수신하는 상태가 되도록 Listen()으로 변경 -> Socket이 요청을 수락하도록 while문을 통해 accept()를 반복 -> Socket이 연결되면 StreamWriter 객체 생성 후 리스트에 넣음 -> 이후 메세지 주고 받기
+    
     class Program
     {
+        static List<StreamWriter> clientWriters = new List<StreamWriter>();
+
         static void Main(string[] args)
         {
             var endPoint = new IPEndPoint(IPAddress.Any, 5555);
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
-            {   
+            {
                 socket.Bind(endPoint);
                 socket.Listen();
                 Console.WriteLine("Listener 동작 시작");
@@ -22,8 +30,13 @@ namespace Sock_console_server
                     Socket client = socket.Accept();
                     Console.WriteLine("클라이언트의 연결 대기 및 수락");
 
+                    NetworkStream NS = new NetworkStream(client);
+                    StreamWriter SW = new StreamWriter(NS, Encoding.UTF8);
+
+                    clientWriters.Add(SW);
+
                     // 클라이언트를 메인 스레드와 다른 별도의 스레드로 처리
-                    Task.Run(() => HandleClient(client));
+                    Task.Run(() => HandleClient(client, SW));
                 }
             }
             catch (Exception e)
@@ -32,21 +45,24 @@ namespace Sock_console_server
             }
         }
 
-        static void HandleClient(Socket client)
+        static void HandleClient(Socket client, StreamWriter writer)
         {
-            StreamReader SR = null;
-            StreamWriter SW = null;
-            Guid uuid = Guid.NewGuid();
+            StreamReader SR = new StreamReader(new NetworkStream(client), Encoding.UTF8);
+            string nickname = "예비닉네임";
+
+            while (nickname == "예비닉네임")
+            {
+                nickname = SR.ReadLine();
+            }
+            
+            foreach (var clientWriter in clientWriters)
+            {
+                clientWriter.WriteLine("{0}님이 채팅방에 입장하셨습니다.", nickname);
+                clientWriter.Flush();
+            }
 
             try
             {
-                NetworkStream NS = new NetworkStream(client);
-                Console.WriteLine("클라이언트와 통신하는 네트워크 스트림 생성");
-                SR = new StreamReader(NS, Encoding.UTF8);
-                Console.WriteLine("네트워크 스트림으로부터 메시지를 가져오는 스트림 생성");
-                SW = new StreamWriter(NS, Encoding.UTF8);
-                Console.WriteLine("네트워크 스트림으로 메시지를 보내는 스트림 생성");
-
                 while (client.Connected)
                 {
                     string GetMessage = SR.ReadLine();
@@ -54,23 +70,33 @@ namespace Sock_console_server
                         break;
 
                     Console.WriteLine("클라이언트로부터 메시지 읽기");
-                    SW.WriteLine("{0}: {1}", uuid.ToString(), GetMessage);
+
+                    string formattedMessage = string.Format("{0}: {1}", nickname, GetMessage);
+
+                    // 모든 클라이언트에게 브로드캐스트
+                    foreach (var clientWriter in clientWriters)
+                    {
+                        clientWriter.WriteLine(formattedMessage);
+                        clientWriter.Flush();
+                    }
+
                     Console.WriteLine("클라이언트에게 메시지 보내기");
-                    SW.Flush();
                     Console.WriteLine("버퍼의 내용을 클라이언트로 보냄");
-                    Console.WriteLine("{0}: {1}", uuid.ToString(), GetMessage);
+                    Console.WriteLine(formattedMessage);
                 }
             }
             catch (Exception e)
             {
-                System.Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message);
             }
             finally
             {
-                if (SW != null)
-                    SW.Close();
-                if (SR != null)
-                    SR.Close();
+                if (writer != null)
+                {
+                    clientWriters.Remove(writer);
+                    writer.Close();
+                }
+                SR.Close();
                 client.Close();
                 Console.WriteLine("연결 해제");
             }
