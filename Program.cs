219dca033e3dc209afd91eab5dfe5f817bd0511c
Declan <declan@supertree.co>
2023-07-26 17:58:00 +0900

    Socket만 이용하여 통신하도록 변경

--- Program.cs
+++ Program.cs
@@ -9,34 +9,45 @@ using System.Threading.Tasks;
 namespace Sock_console_server
 {
     // 서버의 흐름: 소켓 생성 -> bind()로 소켓에 주소 할당 -> Socket이 연결 시도를 수신하는 상태가 되도록 Listen()으로 변경 -> Socket이 요청을 수락하도록 while문을 통해 accept()를 반복 -> Socket이 연결되면 StreamWriter 객체 생성 후 리스트에 넣음 -> 이후 메세지 주고 받기
-    
+    // struct / class 의 차이점.
+    //   value type: 복사값으로 넘어감.
+    //   class type: 레퍼런스로 넘어감.
+    // Boxing / UnBoxing
+
     class Program
     {
-        static List<StreamWriter> clientWriters = new List<StreamWriter>();
+        private static List<object> _list = new ();
+        private static List<Socket> clients = new List<Socket>();
 
         static void Main(string[] args)
         {
-            var endPoint = new IPEndPoint(IPAddress.Any, 5555);
+            // 어떤 주소든 접근할 수 있도록 EndPoint 객체 생성
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 5555);
+            //  TCP 프로토콜을 사용하여 연결 지향 데이터 스트림을 구현하는 소켓을 생성
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
             {
+                // socket 객체에 앤드포인트 설정
                 socket.Bind(endPoint);
+                // socket에 클라이언트가 접근할 수 있도록 설정
                 socket.Listen();
                 Console.WriteLine("Listener 동작 시작");
-
+                
                 while (true)
                 {
+                    // 클라이언트의 요청을 승인하여 메세지 송수신 준비
                     Socket client = socket.Accept();
                     Console.WriteLine("클라이언트의 연결 대기 및 수락");
 
-                    NetworkStream NS = new NetworkStream(client);
-                    StreamWriter SW = new StreamWriter(NS, Encoding.UTF8);
-
-                    clientWriters.Add(SW);
+                    var buffer = new Memory<byte>(new byte[1024]);
+                    ValueTask<int> receive = socket.ReceiveAsync(buffer);
+                    
+                    // 서버로 데이터가 들어왔을 때 모든 참가자들에게 메세지를 전달하기 위해 StreamWriter를 List에 담음
+                    clients.Add(client);
 
                     // 클라이언트를 메인 스레드와 다른 별도의 스레드로 처리
-                    Task.Run(() => HandleClient(client, SW));
+                    Task.Run(() => HandleClient(client));
                 }
             }
             catch (Exception e)
@@ -45,39 +56,46 @@ namespace Sock_console_server
             }
         }
 
-        static void HandleClient(Socket client, StreamWriter writer)
+        static async Task HandleClient(Socket client)
         {
-            StreamReader SR = new StreamReader(new NetworkStream(client), Encoding.UTF8);
+            // 버퍼 메모리 설정
+            var buffer = new Memory<byte>(new byte[1024]);
             string nickname = "예비닉네임";
 
+            // 스트림에 처음 들어온 문자열은 닉네임으로 지정
             while (nickname == "예비닉네임")
             {
-                nickname = SR.ReadLine();
+                int receivedBytes = await client.ReceiveAsync(buffer);
+                nickname = Encoding.UTF8.GetString(buffer.Span.Slice(0, receivedBytes));
             }
             
-            foreach (var clientWriter in clientWriters)
+            // List에 담긴 StreamWriter들 각각 입장 알림 전송
+            foreach (var socket in clients)
             {
-                clientWriter.WriteLine("{0}님이 채팅방에 입장하셨습니다.", nickname);
-                clientWriter.Flush();
+                string welcomeMessage = string.Format("{0}님이 채팅방에 입장하셨습니다.", nickname);
+                buffer = Encoding.UTF8.GetBytes(welcomeMessage);
+                await socket.SendAsync(buffer);
             }
 
             try
             {
                 while (client.Connected)
                 {
-                    string GetMessage = SR.ReadLine();
-                    if (GetMessage == null)
+                    int receivedBytes = await client.ReceiveAsync(buffer);
+                    if (receivedBytes == 0)
                         break;
 
                     Console.WriteLine("클라이언트로부터 메시지 읽기");
 
-                    string formattedMessage = string.Format("{0}: {1}", nickname, GetMessage);
+                    string getMessage = Encoding.UTF8.GetString(buffer.Span.Slice(0, receivedBytes));
+                    string formattedMessage = string.Format("{0}: {1} - {2}", nickname, getMessage, DateTime.Now.ToString("HH:mm:ss"));
 
-                    // 모든 클라이언트에게 브로드캐스트
-                    foreach (var clientWriter in clientWriters)
+                    foreach (var socket in clients)
                     {
-                        clientWriter.WriteLine(formattedMessage);
-                        clientWriter.Flush();
+                        // String을 바이트 코드로 변환
+                        byte[] formattedBuffer = Encoding.UTF8.GetBytes(formattedMessage);
+                        // 변환된 바이트 코드를 보내준다
+                        await socket.SendAsync(formattedBuffer);
                     }
 
                     Console.WriteLine("클라이언트에게 메시지 보내기");
@@ -91,13 +109,11 @@ namespace Sock_console_server
             }
             finally
             {
-                if (writer != null)
+                if (clients.Contains(client))
                 {
-                    clientWriters.Remove(writer);
-                    writer.Close();
+                    clients.Remove(client);
+                    client.Close();
                 }
-                SR.Close();
-                client.Close();
                 Console.WriteLine("연결 해제");
             }
         }
